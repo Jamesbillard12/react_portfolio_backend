faefe9eddabb86a6d5fb1650e39174ae671165c2
jamesbillard <jamesbillard12@hotmail.com>
2018-06-21 11:48:38 -0700

    Fixed cors issue

--- server.js
+++ server.js
@@ -3,17 +3,23 @@
 const express = require('express')
 const debug = require('debug')('profile:server')
 const dotenv = require('dotenv')
+dotenv.load()
 const morgan = require('morgan')
 const cors = require('cors')
 const util = require('util')
-const db = require('./lib/db')
 
 const project = require('./routes/project-routes.js')
-
 const app = express()
 const PORT = process.env.PORT || 3000
+console.log(PORT)
+const CORS_ORIGINS = process.env.CORS_ORIGINS
 
-app.use(cors())
+app.use(
+	cors({
+		origin: CORS_ORIGINS.split(' '),
+		credentials: true
+	})
+)
 app.use(morgan('dev'))
 
 app.use(project)
